c7d5b72afc92c81722cc3619487b01f6344d72b4
Shunki Takami <s-takami@tkms.jp>
2023-10-17 21:22:53 +0900

    fix git_rm...

--- git_rm_file_from_history.sh
+++ git_rm_file_from_history.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export FILTER_BRANCH_SQUELCH_WARNING=1
-if [ "$1" != ""];then
+if [ "$1" != "" ];then
 git filter-branch --index-filter "git rm --cached --ignore-unmatch $1" --tag-name-filter 'cat' -- --all
 fi
 git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
