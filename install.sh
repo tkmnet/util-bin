5172bffd36f75a69a3590bcd6b47617793fa2677
Shunki Takami <tkmnet@users.noreply.github.com>
2025-02-13 16:35:22 +0900

    fix install.sh and vishell

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ cd $(dirname $0)
 
 CONFIG_LINE="export PATH=\"$(pwd | sed -e "s%${HOME}%~%"):\${PATH}\""
 
+if [ -d ~/.bashrc.d ];then
+  echo $CONFIG_LINE > ~/.bashrc.d/util-bin
+else
 if [ $(grep "$CONFIG_LINE" ~/.bashrc | wc -l) -lt 1 ];then
   echo $CONFIG_LINE >> ~/.bashrc
+fi
 fi
 
